2e5a8cdfae5d77977afab190b3167695f0f33f2a
Mukesh Singh <contact@techsdl.com>
2017-07-14 03:03:47 +0530

    Added Constant.java file
    Define some required parameters here.

---
+++ app/src/main/java/com/scionous/samplepushnotification/Constanct.java
@@ -0,0 +1,14 @@
+package com.scionous.samplepushnotification;
+
+/**
+ * Created by Mukesh on 13-Jul-17.
+ */
+
+public class Constant {
+
+    public static String BASE_URL = "https://api.scionous.com/demo/get-push-notification";
+    public static String SERVER_KEY = "REPLCE-FIREBASE-SERVER-KEY";
+    public static String GCM_KEY = "gcm_access";
+    public static String SHARED = "scionous_push_demo";
+
+}
